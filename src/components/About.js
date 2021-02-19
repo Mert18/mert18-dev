d463f7e49844a827e15ba48844d2ce06e42dad23
Mert <mertuygur02@gmail.com>
2021-02-19 23:10:02 +0300

    what just happened

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import '../styles/main.scss'
 
+import linux from '../assets/stack/linux.png';
+import nodejs from '../assets/stack/node-js.png';
+import reactjs from '../assets/stack/react-js.png';
+import sass from '../assets/stack/sass.png';
+
+import ill from '../assets/stack/ill.png'
+
+import egg from '../assets/other/egg.png'
+
 const About = () => {
     return (
         <div className="about">
-            about page
+            <section className="about__whatiknow">
+                <div className="about__whatiknow__web">
+                    <div className="about__whatiknow__web__text">
+                        <h1>what i know?</h1>
+                    </div>
+                    <div className="about__whatiknow__web__tech">
+                        <img src={reactjs} alt="react" width="150px" />
+                        <img src={nodejs} alt="nodejs" width="150px" />
+                        <img src={sass} alt="sass" width="150px" />
+                        <img src={linux} alt="linux" width="150px" />
+                    </div>
+                    <div className="about__whatiknow__web__other">
+                        <img src={ill} alt="ill" width="150px" />
+                    </div>
+                </div>
+                <div className="about__whatiknow__illone">
+                    <img src={egg} alt="egg" />
+                </div>
+            </section>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/main.scss';
 
-import pp from '../assets/pp.svg';
-import linkedin from '../assets/link.png';
-import github from '../assets/git.png'
+import pp from '../assets/other/pp.svg';
+import linkedin from '../assets/other/link.png';
+import github from '../assets/other/git.png'
 
 import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
     return (
@@ -30,7 +31,7 @@ const Header = () => {
 
 
             <div className="header__links">
-                <a className="header__links__linkedin" href="https://www.linkedin.com/in/mert-u-8248ab135/" target="_blank" rel="noreferrer" >
+                <a className="header__links__linkedin" href="https://www.linkedin.com/in/mert-u-8248ab135/" target="_blank" rel="noreferrer">
                     <img src={linkedin} alt="linkedin" width="40px" />
                 </a>
                 <a className="header__links__github" href="https://github.com/Mert18" target="_blank" rel="noreferrer" >
