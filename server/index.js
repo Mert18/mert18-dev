edff26f527006bd912ebb6018c44c176b8e41291
Mert <mertuygur02@gmail.com>
2021-02-25 14:36:22 +0300

    pass removed

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://hachiko2k:hachiko2k@portfolio-data.wii6t.mongodb.net/portfolio-data?retryWrites=true&w=majority';
+const CONNECTION_URL = '';
 
 const PORT = process.env.PORT || 5000;
 
