f3aea7a8e53ec57eaa020c25d9c1473960d7b434
Mert <mertuygur02@gmail.com>
2021-03-01 21:00:48 +0300

    projects layout will be handled

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Project from './Project'
 
-import firstbg from '../assets/projects/one.png';
 
 const Projects = () => {
     return (
@@ -9,12 +8,18 @@ const Projects = () => {
             <div className="projects__text">
                 <h1>some of my projects</h1>
             </div>
+
             <div className="projects__projects">
-                <Project title="Virtual Library" text="A social media platform for book lovers." />
-                <Project title="Virtual Library" text="A social media platform for book lovers." />
-                <Project title="Virtual Library" text="A social media platform for book lovers." />
-                <Project title="Virtual Library" text="A social media platform for book lovers." />
+                <div className="col-2">
+                    <Project title="Virtual Library" text="A social media platform for book lovers." />
+
+                    <div className="col-2">
+                        <Project title="Virtual Library" text="A social media platform for book lovers." />
+                        <Project title="Virtual Library" text="A social media platform for book lovers." />
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
