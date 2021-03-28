4f97f13e6bfe97ad482cd6ba9b525310379225e5
Mert <mertuygur02@gmail.com>
2021-03-28 22:22:58 +0300

    ilham geldi

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../styles/main.scss';
 
-import pp from '../assets/other/pp.png';
 import linkedin from '../assets/other/link.png';
 import github from '../assets/other/git.png'
 
@@ -11,17 +10,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <div className="header">
-            <div className="header__profile">
-                <div className="header__profile__img">
-                    <img src={pp} alt="profile pic" width="150px" draggable="false" />
-                </div>
-                <div className="header__profile__text">
-                    <h1>Mert Uygur</h1>
-                    <h2>Computer Engineering Student</h2>
-                    <h2>Ankara, Turkey</h2>
-                    <h2>Front End Developer</h2>
-                </div>
-            </div>
+
             <div className="header__navigation">
                 <NavLink exact to="/" activeClassName="selected">Home</NavLink>
                 <NavLink to="/about" activeClassName="selected">About</NavLink>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import TopImage from './TopImage'
+import pp from '../assets/other/pp.png';
+import { NavLink } from 'react-router-dom';
+
 
 
 const Home = () => {
     return (
         <div className="home">
-            <div className="container__main__topImage">
-                <TopImage />
-            </div>
+
+            <section className="home__profile">
+                <div className="home__profile__img">
+                    <img src={pp} alt="profile pic" width="150px" draggable="false" />
+                </div>
+                <div className="home__profile__text">
+                    <h1>Mert Uygur</h1>
+                    <h2>Computer Engineering Student</h2>
+                    <h2>Ankara, Turkey</h2>
+                    <h2>Full Stack Developer</h2>
+                </div>
+            </section>
+
+            <section className="home__blog">
+                <div className="home__blog__title">
+                    <NavLink to="/posts"><h2>Blog</h2></NavLink>
+                </div>
+                <div className="home__blog__featuredblog">
+
+                </div>
+            </section>
+
+            {/*<TopImage />*/}
         </div>
     )
 }
